ed4a59c55f735a30fd621b29e610f1e79e83d316
Tejal Par <parulekar.t@gmail.com>
2017-10-17 23:41:17 -0700

    add firebase dbhelper for realtime CRUD operations

---
+++ VolunteerHero/app/src/main/java/com/codepath/volunteerhero/database/FirebaseDBHelper.java
@@ -0,0 +1,49 @@
+package com.codepath.volunteerhero.database;
+
+import com.codepath.volunteerhero.models.Event;
+import com.codepath.volunteerhero.models.User;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+/**
+ * Created by tejalpar
+ */
+public class FirebaseDBHelper {
+    public static String TAG = FirebaseDBHelper.class.getSimpleName();
+
+    private static final String USERS_NODE = "users";
+    private static final String EVENTS_NODE = "events";
+    private static final String CARRIER_NODE = "carrier";
+
+    private static FirebaseDBHelper instance;
+    private static FirebaseClient firebaseClient;
+
+    public static FirebaseDBHelper getInstance() {
+        if (instance == null) {
+            instance = new FirebaseDBHelper();
+        }
+        return instance;
+    }
+
+    private FirebaseDBHelper() {
+        firebaseClient = FirebaseClient.getInstance();
+    }
+
+    public void saveUser(FirebaseUser user) {
+        // add or update user
+        User dbUser = new User(user.getUid(), user.getDisplayName(), user.getEmail(), user.getPhotoUrl().getPath());
+        firebaseClient.getFirebaseDatabase().child(USERS_NODE).child(user.getUid()).setValue(dbUser);
+    }
+
+    public void addEvent(Event event) {
+        DatabaseReference dfReference = firebaseClient.getFirebaseDatabase();
+
+        event.id = dfReference.child(EVENTS_NODE).push().getKey();
+        event.carrier.id = dfReference.child(EVENTS_NODE).child(CARRIER_NODE).push().getKey();
+        dfReference.child(EVENTS_NODE).child(event.id).setValue(event);
+    }
+
+    public void updateEvent(Event event) {
+        firebaseClient.getFirebaseDatabase().child(EVENTS_NODE).child(event.id).setValue(event);
+    }
+}
