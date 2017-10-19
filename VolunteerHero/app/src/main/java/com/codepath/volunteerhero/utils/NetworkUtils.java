cf1ad9a6e386c77175a36cfd6d9389c883382437
Jan Spidlen <jan.spidlen@airbnb.com>
2017-10-18 23:37:29 -0700

    Adding NetworkUtils with error reporting.

---
+++ VolunteerHero/app/src/main/java/com/codepath/volunteerhero/utils/NetworkUtils.java
@@ -0,0 +1,38 @@
+package com.codepath.volunteerhero.utils;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
+import com.codepath.volunteerhero.R;
+
+/**
+ * Created by jan_spidlen on 10/18/17.
+ */
+
+public class NetworkUtils {
+
+    public static void showRetryableError(@NonNull View v, @NonNull @StringRes int messageRes,
+                                          @Nullable View.OnClickListener retryAction) {
+        showError(v, messageRes, R.string.retry, retryAction);
+    }
+
+    public static void showNonretryableError(@NonNull View v, @NonNull @StringRes int messageRes,
+                                             @Nullable View.OnClickListener retryAction) {
+        showError(v, messageRes, R.string.ok, retryAction);
+    }
+
+    private static void showError(View v, @StringRes int messageRes,
+                                 @StringRes int buttonAction,
+                                 @Nullable View.OnClickListener retryAction) {
+        final Snackbar snackbar = Snackbar.make(v, messageRes, Snackbar.LENGTH_LONG);
+
+        if (retryAction != null) {
+            snackbar.setAction(buttonAction, retryAction);
+        }
+
+        snackbar.show();
+    }
+}
